5db90262f647f31fc18f8cdf0884ec03d7a2e522
fairhurst-dev <cfairhurst@protonmail.com>
2022-06-22 19:05:13 -0400

    WIP open graph

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import theme from "./theme";
 import { Box } from "@mui/system";
 import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function App() {
   const imgURL =
@@ -19,6 +20,46 @@ function App() {
       : "https://fairhurstdevassets.s3.amazonaws.com/neonMobile.png";
   return (
     <BrowserRouter>
+      <Helmet>
+        <meta charset="utf-8" />
+        <meta
+          property="og:image"
+          content="https://fairhurstdevassets.s3.amazonaws.com/icon.png"
+        />
+        <link
+          rel="icon"
+          href="https://fairhurstdevassets.s3.amazonaws.com/icon.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://fairhurstdevassets.s3.amazonaws.com/icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://fairhurstdevassets.s3.amazonaws.com/icon.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://fairhurstdevassets.s3.amazonaws.com/icon.png"
+          sizes="16x16"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="https://fairhurstdevassets.s3.amazonaws.com/icon.png"
+          color="#5bbad5"
+        />
+        <meta
+          name="description"
+          content="fairhurst.dev by Caroline Fairhurst"
+        />
+      </Helmet>
       <ThemeProvider theme={theme}>
         <Box
           sx={{
--- src/components/pages/Mirror.js
+++ src/components/pages/Mirror.js
@@ -6,6 +6,7 @@ import SuppliesList from "../mirror/SuppliesList";
 import Hardware from "../mirror/Hardware";
 import Software from "../mirror/Software";
 import Intro from "../mirror/Intro";
+import { Helmet } from "react-helmet";
 
 function Mirror() {
   const dividerStyle = {
@@ -28,65 +29,84 @@ function Mirror() {
   };
 
   return (
-    <Grid
-      justifyContent="center"
-      sx={{
-        p: 2,
-        justifyContent: "center",
-        "@media (max-width: 800px)": { px: 2 },
-      }}
-    >
+    <>
       {" "}
-      <Grid xs={12} display="flex" justifyContent="center" item container>
-        <Typography
-          sx={titleStyle}
-          variant="h2"
-          align="center"
-          color="text.primary"
-        >
-          Building a Magic Mirror
-        </Typography>
-      </Grid>
-      <Grid item container display="flex" justifyContent="center" xs={12}>
-        <Grid item md={10} xs={12}>
-          <Paper
-            sx={{
-              backgroundColor: "transparent",
-              height: "max-content",
-              m: 2,
-              p: 1,
-            }}
-            elevation={10}
+      <Helmet>
+        <title>Building A Magic Mirror</title>
+        <meta property="og:title" content="Building A Magic Mirror" />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:url"
+          content="https://dev.fairhurst.dev/portfolio/mirror"
+        />
+        <meta
+          property="og:image"
+          content="https://fairhurstdevassets.s3.amazonaws.com/mirror/magic_mirror_final_full.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="500" />
+        <meta property="og:image:alt" content="Magic mirror full" />
+      </Helmet>{" "}
+      <Grid
+        justifyContent="center"
+        sx={{
+          p: 2,
+          justifyContent: "center",
+          "@media (max-width: 800px)": { px: 2 },
+        }}
+      >
+        <Grid xs={12} display="flex" justifyContent="center" item container>
+          <Typography
+            sx={titleStyle}
+            variant="h2"
+            align="center"
+            color="text.primary"
           >
-            <Typography sx={subtitleStyle} variant="h4">
-              Intro
-            </Typography>
-            <Intro />
-            <Fade in timeout={2500}>
-              <Paper sx={dividerStyle}></Paper>
-            </Fade>
-            <Typography sx={subtitleStyle} variant="h4">
-              Supplies
-            </Typography>
-            <SuppliesList />
-            <Fade in timeout={2500}>
-              <Paper sx={dividerStyle}></Paper>
-            </Fade>
-            <Typography sx={subtitleStyle} variant="h4">
-              Software
-            </Typography>
-            <Software />
-            <Fade in timeout={2500}>
-              <Paper sx={dividerStyle}></Paper>
-            </Fade>
-            <Typography sx={subtitleStyle} variant="h4">
-              Hardware
-            </Typography>
-            <Hardware />
-          </Paper>
+            Building a Magic Mirror
+          </Typography>
+        </Grid>
+        <Grid item container display="flex" justifyContent="center" xs={12}>
+          <Grid item md={10} xs={12}>
+            <Paper
+              sx={{
+                backgroundColor: "transparent",
+                height: "max-content",
+                m: 2,
+                p: 1,
+              }}
+              elevation={10}
+            >
+              <Typography sx={subtitleStyle} variant="h4">
+                Intro
+              </Typography>
+              <Intro />
+              <Fade in timeout={2500}>
+                <Paper sx={dividerStyle}></Paper>
+              </Fade>
+              <Typography sx={subtitleStyle} variant="h4">
+                Supplies
+              </Typography>
+              <SuppliesList />
+              <Fade in timeout={2500}>
+                <Paper sx={dividerStyle}></Paper>
+              </Fade>
+              <Typography sx={subtitleStyle} variant="h4">
+                Software
+              </Typography>
+              <Software />
+              <Fade in timeout={2500}>
+                <Paper sx={dividerStyle}></Paper>
+              </Fade>
+              <Typography sx={subtitleStyle} variant="h4">
+                Hardware
+              </Typography>
+              <Hardware />
+            </Paper>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
 
