1cdbfcaefe56650a435755bdf72d2bb36550cbc5
fairhurst-dev <cfairhurst@protonmail.com>
2024-09-14 16:22:46 -0400

    WIP 2024 refresh
    no sls

--- src/lib/CardData.js
+++ src/lib/CardData.js
@@ -5,6 +5,12 @@ const cardStyle = { color: "secondary.main", fontSize: 200 };
 
 const cardData = [
   {
+    title: "Resume",
+    description: ["Click to download"],
+    icon: <SaveAlt sx={cardStyle} />,
+    link: "",
+  },
+  {
     title: "Magic Mirror",
     description: ["Read about my Magic Mirror build"],
     icon: (
@@ -23,12 +29,6 @@ const cardData = [
     description: ["Check out my repos"],
     icon: <GitHub sx={cardStyle} />,
     link: "https://github.com/fairhurst-dev/",
-  },
-  {
-    title: "Resume",
-    description: ["Click to download"],
-    icon: <SaveAlt sx={cardStyle} />,
-    link: "",
   },
 ];
 
