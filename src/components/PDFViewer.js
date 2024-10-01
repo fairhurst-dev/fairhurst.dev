73b9217ec73e489f790e886d414c2ebbec77ac2b
C <c@fairhursts-mbp.mynetworksettings.com>
2024-10-01 17:05:13 -0400

    wip use react-pdf

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,51 +1,16 @@
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import { getFilePlugin } from "@react-pdf-viewer/get-file";
+import { Document } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
 
-function PDFViewer(props) {
-  const getFilePluginInstance = getFilePlugin({
-    fileNameGenerator: () => {
-      return `caroline-fairhurst-resume`;
-    },
-  });
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
-  const { DownloadButton } = getFilePluginInstance;
+function PDFViewer() {
 
   return (
-    <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js">
-      <div
-        style={{
-          alignItems: "center",
-          backgroundColor: "#eeeeee",
-          borderBottom: "1px solid rgba(0, 0, 0, 0.3)",
-          display: "flex",
-          padding: "4px",
-        }}
-      >
-        <div
-          style={{
-            padding: "5px",
-          }}
-        >
-          <DownloadButton />
-        </div>
-      </div>
-      <div
-        style={{
-          border: "1px solid rgba(0, 0, 0, 0.3)",
-          height: "600px",
-        }}
-      >
-        <Viewer
-          theme={{
-            theme: "dark",
-          }}
-          plugins={[getFilePluginInstance]}
-          fileUrl="/assets/resume.pdf"
-        />
-      </div>
-    </Worker>
-  );
+    <Document file="/assets/resume.pdf" />
+  )
 }
 
 export default PDFViewer;
